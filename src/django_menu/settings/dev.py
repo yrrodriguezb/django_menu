c7859d81498ddbebed746e8506077755248b4388
Yeison Rodriguez <yrrodriguezb@gmail.com>
2021-08-25 23:13:22 -0500

    Estrcutura de carpetas y archivos del proyecto

---
+++ src/django_menu/settings/dev.py
@@ -0,0 +1,20 @@
+from .base import *
+
+DEBUG = True 
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default':{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Applications 
+
+APPS_THIRD_PARTY = []  
+
+APPS = []
+
+INSTALLED_APPS = DJANGO_APPS + APPS_THIRD_PARTY + APPS
---
+++ src/django_menu/settings/prod.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEBUG = False 
+
+ALLOWED_HOSTS = ['*']
+
+# Applications 
+
+APPS_THIRD_PARTY = []  
+
+APPS = []
+
+INSTALLED_APPS = DJANGO_APPS + APPS_THIRD_PARTY + APPS
